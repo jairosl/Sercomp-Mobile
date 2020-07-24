a82fb888f30127a696210bea4ed3125d2ec5f25d
Márcio <marciobwfilho98@gmail.com>
2020-07-24 16:57:50 -0300

    Criando tela principal de informações

--- src/screen/Info/index.jsx
+++ src/screen/Info/index.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text, ScrollView, TouchableOpacity } from 'react-native';
+import logo from '../../assets/LGReduzida.png';
 
 import styles from './style';
 
 const Info = () => {
   return (
-    <>
-      <View style={styles.container} />
-    </>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.header}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>Olá Usuário</Text>
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardLabel}>Nome</Text>
+            <Text style={styles.cardData}>Márcio Bizerra Wanderley Filho</Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardLabel}>Email</Text>
+            <Text style={styles.cardData}>marciobwfilho98@gmail.com</Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardLabel}>Curso</Text>
+            <Text style={styles.cardData}>Ciência da Computação</Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardLabel}>Universidade</Text>
+            <Text style={styles.cardData}>UEPB</Text>
+          </View>
+          <View style={styles.cardContainer}>
+            <Text style={styles.cardLabel}>Minicursos</Text>
+            <Text style={styles.cardData}>2</Text>
+          </View>
+        </View>
+
+        <View style={styles.containerButton}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sair</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
--- src/screen/Info/style.js
+++ src/screen/Info/style.js
@@ -1,9 +1,81 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
 
 const style = StyleSheet.create({
   container: {
+    alignItems: 'center',
     flex: 1,
     backgroundColor: '#5C3BA0',
+    width: windowWidth,
+  },
+
+  header: {
+    alignItems: 'center',
+    marginTop: 60,
+    marginBottom: 10,
+  },
+
+  logo: {
+    height: 80,
+  },
+
+  title: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+
+  titleText: {
+    color: 'white',
+    fontSize: 28,
+    letterSpacing: 0.5,
+  },
+
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    width: windowWidth - 50,
+    padding: 10,
+    paddingLeft: 15,
+  },
+
+  cardContainer: {
+    marginVertical: 5,
+  },
+
+  cardLabel: {
+    color: '#5C3BA0',
+    letterSpacing: 0.5,
+    fontSize: 22,
+  },
+
+  cardData: {
+    color: 'gray',
+    fontSize: 20,
+    letterSpacing: 0.5,
+  },
+
+  containerButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
+  // '#BE8E00
+  button: {
+    backgroundColor: 'red',
+    height: 40,
+    width: 180,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginTop: 30,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+    letterSpacing: 0.5,
   },
 });
 
