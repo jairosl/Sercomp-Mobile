09443ad98d88fee1870b1cc06d2334f7d45a224e
jairosl <jairo.lima@aluno.uepb.edu.br>
2020-07-27 16:08:47 -0300

    Melhorando o style da Tabbar

--- src/routes/routestab.jsx
+++ src/routes/routestab.jsx
@@ -15,8 +15,13 @@ export default function RoutesTab() {
       tabBarOptions={{
         activeTintColor: 'white',
         inactiveTintColor: 'gray',
-        activeBackgroundColor: '#5C3BA0',
-        inactiveBackgroundColor: '#5C3BA0',
+        style: {
+          backgroundColor: '#5C3BA0',
+          height: 60,
+          paddingBottom: 7,
+          fontSize: 10,
+        },
+        labelStyle: { fontSize: 11 },
       }}
     >
       <Tab.Screen
@@ -33,7 +38,7 @@ export default function RoutesTab() {
         name="minicurso"
         component={Minicursos}
         options={{
-          tabBarLabel: 'Seus Minicursos',
+          tabBarLabel: 'Meus Minicursos',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="teach" size={size} color={color} />
           ),
